752fca5ba2232d876334e55bf6dc08ae304399dc
abhilash <abhimadaparambath@gmail.com>
2023-08-10 13:15:54 +0530

    updates3

--- frontend/src/axios/axiosInstance.jsx
+++ frontend/src/axios/axiosInstance.jsx
@@ -3,7 +3,7 @@ const userToken = localStorage.getItem('jwt');
 const adminToken = localStorage.getItem('adminjwt');
 const userInstance = axios.create({
     baseURL: import.meta.env.VITE_SERVER_URL,
-    timeout: 5000,
+   
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${userToken}`
@@ -12,7 +12,7 @@ const userInstance = axios.create({
   });
   const adminInstance = axios.create({
     baseURL: `${import.meta.env.VITE_SERVER_URL}/admin`,
-    timeout: 5000,
+   
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${adminToken}`
